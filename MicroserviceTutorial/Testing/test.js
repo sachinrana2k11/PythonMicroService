31036fe1c24fe5635479eb46ae6f5133a9cc6b8e
Sachin rana <sachin.rana@live.com>
2023-09-17 23:18:32 +0530

    Update test.js

--- MicroserviceTutorial/Testing/test.js
+++ MicroserviceTutorial/Testing/test.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('https://ip/news?country=us');
+  http.get('https://192.168.xx.xxx/news?country=us');
   sleep(1);
 }
